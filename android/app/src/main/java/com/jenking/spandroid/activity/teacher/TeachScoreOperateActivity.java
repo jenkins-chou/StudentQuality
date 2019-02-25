8881d4e226421082b16f69ded45387eda7d2ed53
jenkins <13413607283@163.com>
2019-02-25 10:14:21 +0800

    33

--- android/app/src/main/java/com/jenking/spandroid/activity/teacher/TeachScoreOperateActivity.java
+++ android/app/src/main/java/com/jenking/spandroid/activity/teacher/TeachScoreOperateActivity.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.library.BaseRecyclerAdapter;
 import com.github.library.BaseViewHolder;
@@ -17,12 +22,14 @@ import com.jenking.spandroid.activity.common.CourseDetailActivity;
 import com.jenking.spandroid.activity.manager.ManagerCourseActivity;
 import com.jenking.spandroid.api.RS;
 import com.jenking.spandroid.models.base.CourseModel;
+import com.jenking.spandroid.models.base.ResultModel;
 import com.jenking.spandroid.models.impl.UserCourseDetail;
 import com.jenking.spandroid.presenter.CoursePresenter;
 import com.jenking.spandroid.presenter.UserCoursePresenter;
 import com.jenking.spandroid.tools.StringUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,13 +38,23 @@ import butterknife.OnClick;
 
 public class TeachScoreOperateActivity extends BaseActivity {
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
     @OnClick(R.id.back)
     void back(){
         finish();
     }
 
-//    private List<>
+    @OnClick(R.id.submit)
+    void submit(){
+        Toast.makeText(this, scoreMap.toString()+"111", Toast.LENGTH_LONG).show();
+        Log.e("map",scoreMap.toString()+"111");
+    }
+
+    private Map<String,String> scoreMap;
+    private List<UserCourseDetail> userCourseDetails;
     private UserCoursePresenter userCoursePresenter;
+    private BaseRecyclerAdapter baseRecyclerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +64,35 @@ public class TeachScoreOperateActivity extends BaseActivity {
     @Override
     public void initData() {
         super.initData();
+        userCourseDetails = new ArrayList<>();
+        scoreMap = new HashMap<>();
+        baseRecyclerAdapter = new BaseRecyclerAdapter<UserCourseDetail>(this,userCourseDetails,R.layout.activity_score_operate_item) {
+            @Override
+            protected void convert(BaseViewHolder helper, final UserCourseDetail item) {
+                helper.setText(R.id.realname,item.getRealname());
+                helper.setText(R.id.class_name,item.getClass_name());
+                EditText editText = helper.getView(R.id.edittext);
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        scoreMap.put(item.getId(),charSequence.toString());
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+            }
+        };
+        baseRecyclerAdapter.openLoadAnimation(false);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1,1));
+        recyclerView.setAdapter(baseRecyclerAdapter);
         userCoursePresenter = new UserCoursePresenter(this);
         userCoursePresenter.setOnCallBack(new UserCoursePresenter.OnCallBack() {
             @Override
@@ -81,7 +127,14 @@ public class TeachScoreOperateActivity extends BaseActivity {
 
             @Override
             public void getUserByCourseId(boolean isSuccess, Object object) {
-
+                if (isSuccess&&object!=null){
+                    ResultModel resultModel = (ResultModel)object;
+                    if (resultModel!=null&& StringUtil.isEquals(resultModel.getStatus(),"200")){
+                        userCourseDetails.clear();
+                        userCourseDetails = resultModel.getData()!=null?resultModel.getData():userCourseDetails;
+                        baseRecyclerAdapter.setData(userCourseDetails);
+                    }
+                }
             }
         });
 
